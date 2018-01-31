697b01044779960d5c33b0e10a7339ff43390093
Ross Dunford <dunford@Rus-MacBook.local>
2018-01-31 13:07:31 -0700

    completed

--- server/controllers/messages_controller.js
+++ server/controllers/messages_controller.js
@@ -2,36 +2,49 @@ let messages = [];
 let id = 0;
 
 module.exports = {
-  create: ( req, res ) => {
+  create: (req, res) => {
     const { text, time } = req.body;
+    const { user } = req.session;
+
     messages.push({ id, text, time });
+    user.messages.push({ id, text, time });
     id++;
-    res.status(200).send( messages );
+
+    res.status(200).send(messages);
   },
 
-  read: ( req, res ) => {
-    res.status(200).send( messages );
+  read: (req, res) => {
+    res.status(200).send(messages);
   },
 
-  update: ( req, res ) => {
+  update: (req, res) => {
     const { text } = req.body;
-    const updateID = req.params.id;
-    const messageIndex = messages.findIndex( message => message.id == updateID );
-    let message = messages[ messageIndex ];
+    const updateID = req.query.id;
 
-    messages[ messageIndex ] = {
+    const messageIndex = messages.findIndex(message => message.id == updateID);
+    let message = messages[messageIndex];
+
+    messages[messageIndex] = {
       id: message.id,
       text: text || message.text,
       time: message.time
     };
 
-    res.status(200).send( messages );
+    res.status(200).send(messages);
   },
 
-  delete: ( req, res ) => {
-    const deleteID = req.params.id;
-    messageIndex = messages.findIndex( message => message.id == deleteID );
+  delete: (req, res) => {
+    const deleteID = req.query.id;
+
+    messageIndex = messages.findIndex(message => message.id == deleteID);
     messages.splice(messageIndex, 1);
-    res.status(200).send( messages );
+
+    res.status(200).send(messages);
+  },
+
+  history: (req, res) => {
+    const { user } = req.session;
+
+    res.status(200).send(user.messages);
   }
 };
--- server/index.js
+++ server/index.js
@@ -1,17 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mc = require( `./controllers/messages_controller` );
+const mc = require(`./controllers/messages_controller`);
+const session = require('express-session');
+const createInitialSession = require('./middlewares/session');
+const filter = require('./middlewares/filter');
+
+require('dotenv').config();
+
 
 const app = express();
 
-app.use( bodyParser.json() );
-app.use( express.static( `${__dirname}/../build` ) );
+app.use(bodyParser.json());
+app.use(express.static(`${__dirname}/../build`));
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 10000 }
+}))
+
+app.use((req, res, next) => createInitialSession(req, res, next));
+app.use((req, res, next) => {
+    const { method } = req;
+    if (method === 'POST' || method === 'PUT') {
+        filter(req, res, next);
+    }
+    else {
+        next();
+    }
+});
 
 const messagesBaseUrl = "/api/messages";
-app.post( messagesBaseUrl, mc.create );
-app.get( messagesBaseUrl, mc.read );
-app.put( `${messagesBaseUrl}`, mc.update );
-app.delete( `${messagesBaseUrl}`, mc.delete );
+app.post(messagesBaseUrl, mc.create);
+app.get(messagesBaseUrl, mc.read);
+app.get(`${messagesBaseUrl}/history`, mc.history)
+app.put(`${messagesBaseUrl}`, mc.update);
+app.delete(`${messagesBaseUrl}`, mc.delete);
 
 const port = process.env.PORT || 3000
-app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
+app.listen(port, () => { console.log(`Server listening on port ${port}.`); });
